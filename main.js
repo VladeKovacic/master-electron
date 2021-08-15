f912b62bf2f96092c6367ea9952df32db767b2df
Vlade Kovacic <kovacic.vlade@feg.eu>
2021-08-15 17:15:51 +0200

    Initial commit

--- main.js
+++ main.js
@@ -1,29 +1,54 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
+const windowStateKeeper = require('electron-window-state')
+const readItem = require('./readItem')
+const appMenu = require('./menu')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+// Listen for new item request
+ipcMain.on('new-item', (e, itemUrl) => {
+  //Get new item and send back to renderer
+  readItem(itemUrl, item => {
+    e.sender.send('new-item-success', item)
+  })
+})
+
 // Create a new BrowserWindow when `app` is ready
 function createWindow () {
 
+  // Win state keeper
+  let state = windowStateKeeper({
+    defaultWidth: 500,
+    defaultHeight: 650
+  })
+
   mainWindow = new BrowserWindow({
-    width: 1000, height: 800,
+    x: state.x,
+    y: state.y,
+    width: state.width, 
+    height: state.height,
+    minWidth: 350,
+    maxWidth: 650,
+    minHeight: 300,
     webPreferences: {
-      // --- !! IMPORTANT !! ---
-      // Disable 'contextIsolation' to allow 'nodeIntegration'
-      // 'contextIsolation' defaults to "true" as from Electron v12
-      contextIsolation: false,
       nodeIntegration: true
     }
   })
 
+  // Create main app menu
+  appMenu(mainWindow.webContents)
+
   // Load index.html into the new BrowserWindow
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('renderer/main.html')
+
+  // Manage new window state
+  state.manage(mainWindow)
 
   // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools({ mode: 'detach' });
 
   // Listen for window being closed
   mainWindow.on('closed',  () => {
---
+++ renderer/app.js
@@ -0,0 +1,116 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+// Modules
+const { ipcRenderer } = require('electron')
+const items = require('./items')
+
+// Dom nodes
+let showModal = document.getElementById('show-modal'),
+    closeModal = document.getElementById('close-modal'),
+    modal = document.getElementById('modal'),
+    addItem = document.getElementById('add-item'),
+    itemUrl = document.getElementById('url'),
+    search = document.getElementById('search')
+
+// Open modal from menu
+ipcRenderer.on('menu-show-modal', () => {
+    showModal.click()
+})
+
+// Open selected item from menu
+ipcRenderer.on('menu-open-item', () => {
+    items.open()
+})
+
+// Delete selected item from menu
+ipcRenderer.on('menu-delete-item', () => {
+    let selectedItem = items.getSelectedItem()
+    items.delete(selectedItem.index)
+})
+
+// Open item in native browser from menu
+ipcRenderer.on('menu-open-item-native', () => {
+    items.openNative()
+})
+
+// Focus the search input from the menu
+ipcRenderer.on('menu-focus-search', () => {
+    search.focus()
+})
+
+// Filter items with "search"
+search.addEventListener('keyup', e => {
+    // Loop items
+    Array.from(document.getElementsByClassName('read-item')).forEach(item => {
+        // Hide items that don't match search value
+        let hasMatch = item.innerText.toLowerCase().includes(search.value)
+        item.style.display = hasMatch ? 'flex' : 'none'
+    })
+})
+
+// Navigate item selection with up/down arrows
+document.addEventListener('keydown', e => {
+    if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+        items.changeSelection(e.key)
+    }
+})
+
+// Disable and Enable modal buttons
+const toggleModalButtons = () => {
+    if (addItem.disable === true) {
+        addItem.disable = false
+        addItem.style.opacity = 1
+        addItem.innerText = 'Add Item'
+        closeModal.style.display = 'inline'
+    }
+    else {
+        addItem.disable = true
+        addItem.style.opacity = 0.5
+        addItem.innerText = 'Adding...'
+        closeModal.style.display = 'none'
+    }
+}
+
+
+// Show modal
+showModal.addEventListener('click', e => {
+    modal.style.display = 'flex'
+    itemUrl.focus()
+})
+
+// Close modal
+closeModal.addEventListener('click', e => {
+    modal.style.display = 'none'
+})
+
+// Handle new item
+addItem.addEventListener('click', e => {
+    // Check a url exists
+    if (itemUrl.value) {
+        ipcRenderer.send('new-item', itemUrl.value)
+        // Disable buttons
+        toggleModalButtons()
+    }
+})
+
+// Liste for new item from main process
+ipcRenderer.on('new-item-success', (e, newItem) => {
+    // Add new item to "items" node
+    items.addItem(newItem, true)
+
+    // Enable buttons
+    toggleModalButtons()
+
+    // Hide modal and clear value
+    modal.style.display = 'none'
+    itemUrl.value = null
+})
+
+// Listen for keyboard submit
+itemUrl.addEventListener('keyup', e => {
+    if (e.key === 'Enter') {
+        addItem.click()
+    }
+})
